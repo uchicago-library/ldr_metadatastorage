25c7780402e5b747cc0b22e8407fe22335344164
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-08-21 14:35:22 -0500

    issue #5: misc start

--- metadatastorageapi/blueprint/storagelib/filesystemstorage.py
+++ metadatastorageapi/blueprint/storagelib/filesystemstorage.py
@@ -2,10 +2,12 @@ from os import path
 from xml.etree import ElementTree
 
 class FileSystemStorage(object):
-    def __init__(self):
-        self.filepath = path.realpath(path.join("../sandbox", "collections.xml"))
-        self.data = ElementTree.parse(self.filepath)
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.data_root = ElementTree.parse(self.filepath).getroot()
 
     def find_a_particular_collection(self, identifier):
-        match = self.data.getroot().find("{http://lib.uchicago.edu/ldr}collection/{http://purl.org/dc/elements/1.1}identifier=={}".format(identifier))
+        match = self.data_root.find(
+            "{http://lib.uchicago.edu/ldr}collection/{http://purl.org/dc/elements/1.1}identifier=={}".format(identifier))
         print(match.parent())
+
