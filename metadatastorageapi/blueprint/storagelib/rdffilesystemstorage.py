1976d4c8b3d385b674128fada83651ea37e824ec
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-09-06 14:46:46 -0500

    issue #8: added definition for rdf-filesystem

--- metadatastorageapi/blueprint/storagelib/rdffilesystemstorage.py
+++ metadatastorageapi/blueprint/storagelib/rdffilesystemstorage.py
@@ -10,4 +10,16 @@ class RDFFileSystemStorage(StorageSystem):
     def find_root_collections(self):
         root = URIRef("http://metadatastorage.lib.uchicago.edu/collection/ldr")
         [x for x in g.subjects() if x == root]
+        return []
 
+    def find_specific_collection(self, identifier):
+        return []
+
+    def find_extension(self, extension):
+        return None
+
+    def find_collection_extensions(self, collection_id):
+        return []
+
+    def find_core_metadata(self, collection_id):
+        return {}
